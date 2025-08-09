8208ce0c13e7657198502efa6d0aa6eece9dfe17
Maurício Bruno <mb.bruno.s@gmail.com>
2025-08-09 10:44:15 -0300

    feat: add HelloWorld handler and update route to use it

--- backend/internal/infra/http/routes/routes.go
+++ backend/internal/infra/http/routes/routes.go
@@ -3,9 +3,9 @@ package routes
 import (
 	"net/http"
 
-	"github.com/forja-pro/forja-labs-posts/internal/infra/http/controller"
+	"github.com/forja-pro/forja-labs-posts/internal/infra/http/handlers"
 )
 
 func Init() {
-	http.HandleFunc("GET /", controller.HelloWorld)
+	http.HandleFunc("GET /", handlers.HelloWorld)
 }
