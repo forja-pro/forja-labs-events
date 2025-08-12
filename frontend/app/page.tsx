2064bd72de103b2f6b2fce9375662db876372f42
RebecaTI <mebuffbeka@gmail.com>
2025-08-12 18:27:20 -0300

    movendo os arquivos p pasta front end

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import ContentCards from "@/src/components/ContentCards";
+
+export default function Home() {
+  return (
+    <main className="bg-white min-h-screen">
+      <ContentCards />
+    </main>
+  );
+}
