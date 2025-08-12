b8da38eed95503683508318b06df537f0de439cb
RebecaTI <mebuffbeka@gmail.com>
2025-08-12 18:30:45 -0300

    Remove unused files and configurations

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContentCards from "@/src/components/ContentCards";
-
-export default function Home() {
-  return (
-    <main className="bg-white min-h-screen">
-      <ContentCards />
-    </main>
-  );
-}
